af64504dde2892ad021c426cd57e2bb57e0c3ecd
Kristof Jakab <jaksi@users.noreply.github.com>
2020-10-22 17:55:00 +0100

    Move dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setuptools.setup(
     author="Kristof Jakab",
     url="https://github.com/jaksi/deconz_exporter",
     packages=setuptools.find_packages(),
+    install_requires=['requests', 'prometheus-client'],
     entry_points={"console_scripts": ["deconz_exporter = deconz_exporter:main"]},
 )
